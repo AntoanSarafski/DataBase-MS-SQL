77ad9e64bebc0b9b215867761f2be2dcbdf7b13b
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-10-11 22:12:04 +0300

    Update TriggerForAddLogRecordsIntoTable.sql
    Fixed.

--- TriggerForAddLogRecordsIntoTable.sql
+++ TriggerForAddLogRecordsIntoTable.sql
@@ -5,6 +5,7 @@ CREATE OR ALTER TRIGGER tr_OnAccountChangeAddLogRecord
 ON Accounts FOR UPDATE
 AS
 	INSERT AccountChanges (AccountId, OldBalance, NewBalance, DateOfChange)
-	SELECT i.Id, i.balance, i.balance, GETDATE() FROM inserted i
+	SELECT i.Id, d.balance, i.balance, GETDATE() FROM inserted i
 	JOIN deleted d ON i.Id = d.Id
 GO
+
